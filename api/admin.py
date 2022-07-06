1a2a5c8cdb9ebe057e36ff173c13bdb5fe31e328
gkim <kimkm0419@naver.com>
2022-07-07 02:35:48 +0900

    Chore : Add models to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+
+admin.site.register(Category)
+admin.site.register(Type)
+admin.site.register(Product)
+admin.site.register(ProductMedia)
+
+admin.site.register(Review)
+admin.site.register(ReviewMedia)
+
+admin.site.register(SurveyResult)
+
+admin.site.register(Magazine)
+admin.site.register(MagazineContent)
+
+admin.site.register(Brand)
