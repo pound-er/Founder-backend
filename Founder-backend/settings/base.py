5c0d06bea10c919be938ab9cd3dd703ef1d1bfe1
gkim <kimkm0419@naver.com>
2022-07-23 15:31:31 +0900

    🚀 Chore : Add cors-header setting

--- Founder-backend/settings/base.py
+++ Founder-backend/settings/base.py
@@ -116,6 +116,18 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+
 ROOT_URLCONF = 'Founder-backend.urls'
 
 TEMPLATES = [
